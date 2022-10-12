3828fe35edf1bf28d985c4a48f3d400b563675cf
Your Name <you@example.com>
2022-10-11 23:40:38 -0400

    created schema tables

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    dep_titles VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE
+    SET NULL
+): CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER REFERENCES employee(id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE
+    SET NULL
+);
