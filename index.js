7582856ec8e5157ff92e01caf341cf9664cf2929
Your Name <you@example.com>
2022-10-16 22:07:07 -0400

    created delete department function

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const mainMenu = async () => {
         type: "list",
         name: "options",
         message: "Please make a selections",
-        choices: ["View all departments"],
+        choices: ["View all departments", "Delete Department"],
       },
     ])
     .then((choice) => choiceHandler(choice))
@@ -48,6 +48,9 @@ const choiceHandler = async ({ options: choice }) => {
   switch (choice) {
     case "View all departments":
       await getAllDepartments();
+      break;
+    case "Delete Department":
+      await deleteDepartmentHandler();
       break;
   }
   if (choice !== "Quit") {
@@ -61,6 +64,42 @@ const getAllDepartments = async () => {
     .query(`SELECT * FROM departments`)
     .then(([rows]) => console.log(cTable.getTable(rows)))
     .catch((error) => console.log(error));
+};
+const createDepartmentsList = async () => {
+  let departments;
+  await db
+    .promise()
+    .query(`SELECT departments.departmentName FROM departments`)
+    .then(([rows]) => (departments = rows.map((row) => row.name)))
+    .catch((error) => console.log(error));
+  return departments;
+};
+
+const deleteDepartmentHandler = async () => {
+  let departments = await createDepartmentsList();
+  await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "department",
+        message: "Choose department to delete",
+        choices: departments,
+      },
+    ])
+    .then((choice) => deleteDepartment(choice))
+    .catch((error) => console.log(error));
+};
+
+const deleteDepartment = async ({ department }) => {
+  let departmentsArray;
+  await db
+    .promise()
+    .query(`SELECT * FROM departments WHERE name = '${department}'`)
+    .then(([rows]) => (departmentsArray = rows))
+    .catch((error) => console.log(error));
+  if (departmentsArray.length > 1) {
+    console.log(cTable.getTable(departmentsArray));
+  }
 };
 
 const againHandler = async () => {
